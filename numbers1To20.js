4b6a0156cc664a860c82b78dce0b57aae042451a
lentqq <47202527+lentqq@users.noreply.github.com>
2019-02-11 10:31:42 +0200

    upload new files

---
+++ numbers1To20.js
@@ -0,0 +1,6 @@
+function nums() {
+    for (i = 0;i < 20;i ++){
+    console.log(i);}
+}
+
+nums();
---
+++ rectangleArea.js
@@ -0,0 +1,6 @@
+function rectangleArea() {
+    let area = 2 * 7;
+    console.log (area);
+}
+
+rectangleArea();
